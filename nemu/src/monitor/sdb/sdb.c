9a8c47fcdcc63f3527574d56e54da30a98470e26
tracer-ics2023 <tracer@njuics.org>
2023-10-03 14:32:43 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:32:43 up  1:20,  1 user,  load average: 0.26, 0.31, 0.25

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,15 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args){
+	char *arg = strtok(NULL," ");
+	uint64_t n=1;
+	if(arg != NULL){
+		n = atoll(arg);
+	}
+	cpu_exec(n);
+	return 0;
+}
 static struct {
   const char *name;
   const char *description;
@@ -63,7 +72,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+	{ "si", "Single-Step execution", cmd_si},
   /* TODO: Add more commands */
 
 };
