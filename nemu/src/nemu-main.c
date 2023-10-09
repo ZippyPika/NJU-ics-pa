adbf793f7e10c34e203acf06f20214fd5f552708
tracer-ics2023 <tracer@njuics.org>
2023-10-09 21:24:57 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:57 up  8:19,  1 user,  load average: 0.94, 0.55, 0.55

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -22,6 +22,18 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+static char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(nemu) ");
+
+  return line_read;
+}
 int main(int argc, char *argv[])
 {
     /* Initialize the monitor. */
@@ -32,9 +44,7 @@ int main(int argc, char *argv[])
 #endif
     /* Start engine. */
     //  engine_start();
-    char *s = readline("");
-//    printf("2121212\n");
-//    printf("%s\n", s);
+    char *s = rl_gets();
 
     bool *f = 0;
     printf("%u\n",expr(s, f));
