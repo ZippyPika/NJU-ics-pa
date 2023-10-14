3526f8c80bd486152114ec10ab1ac9283c8f8bb1
tracer-ics2023 <tracer@njuics.org>
2023-10-14 20:59:13 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:13 up 19 min,  1 user,  load average: 0.67, 0.66, 0.47

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -20,7 +20,8 @@ const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
+  "pc"
 };
 
 void isa_reg_display() {
@@ -32,10 +33,17 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
     *success=false;
-    for(int i=0;i<32;i++){
+    for(int i=0;i<33;i++){
         if(strcmp(s+1,regs[i])==0){
-            *success=true;
-            return cpu.gpr[i];
+            if(i==32){
+                *success=true;
+                return cpu.pc;
+            }
+            else {
+                *success=true;
+                return cpu.gpr[i];
+            }
+
         }
     }
     return 0;
