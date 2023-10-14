a8b9b135bf2d1e9328ea2545905fed48f7359967
tracer-ics2023 <tracer@njuics.org>
2023-10-14 13:52:27 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:52:27 up 39 min,  1 user,  load average: 1.34, 0.80, 0.72

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -31,5 +31,13 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+    *success=true;
+    int i=0;
+    for(i=0;i<32;++i){
+        if(strcmp(s,regs[i])==0){
+            return cpu.gpr[i];
+        }
+    }
+    *success=false;
+    return 0;
 }
