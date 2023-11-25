be369e78ee099087177fe9b32b5cbaae1a373aed
tracer-ics2023 <tracer@njuics.org>
2023-11-25 11:46:04 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:46:04 up 29 min,  1 user,  load average: 0.63, 0.76, 0.81

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,7 +26,7 @@ $(BINARY):: compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt 
 override ARGS += $(ARGS_DIFF)
-override ARGS += --elf=$(BUILD_DIR)/1.elf
+# override ARGS += --elf=$(BUILD_DIR)/1.elf
 # Command to execute NEMU
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
