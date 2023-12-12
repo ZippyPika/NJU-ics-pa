4ab27f04a3f70dbc09a525d1e48fb719e8993d93
tracer-ics2023 <tracer@njuics.org>
2023-12-12 21:08:47 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:47 up  2:21,  1 user,  load average: 0.67, 0.66, 0.77

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -159,6 +159,7 @@ static int decode_exec(Decode *s) {
     INSTPAT("??????? ????? ????? 010 ????? 11100 11",csrrs, I, R(rd) = CSR(imm); CSR(imm) |= src1);
 
     INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall, N, s->dnpc=isa_raise_intr(R(17), s->pc));
+    INSTPAT("0011000 00010 00000 000 00000 11100 11", mret, N, s->dnpc = CSR(0x341););
     INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
     INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
     INSTPAT_END();
