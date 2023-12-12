ec35e0d5e60db5cfa18d8db0c10799754bc918f8
tracer-ics2023 <tracer@njuics.org>
2023-12-12 20:20:51 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:20:51 up  1:33,  1 user,  load average: 0.89, 0.87, 0.69

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -69,9 +69,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
     IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 //#ifdef CONFIG_WATCHPOINT    
-//    if(scan_watchpoint()){
-//        nemu_state.state=NEMU_STOP;
-//    }
+   if(scan_watchpoint()){
+       nemu_state.state=NEMU_STOP;
+   }
 //#endif
 
 }
