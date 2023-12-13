c7ae0543bf1758cf34c86d886e631ccae03867bd
tracer-ics2023 <tracer@njuics.org>
2023-12-13 20:16:10 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:10 up 55 min,  1 user,  load average: 0.68, 0.57, 0.62

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,25 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+    Elf_Ehdr ehdr;
+    ramdisk_read(&ehdr, 0, sizeof(ehdr));
+    assert(memcmp(ehdr.e_ident, ELFMAG, SELFMAG) == 0);
+
+    Elf_Phdr phdr;
+    for (int i = 0; i < ehdr.e_phnum; i++) {
+        // 计算 Program Header 的位置并读取它
+        int phoff = ehdr.e_phoff + i * ehdr.e_phentsize;
+        ramdisk_read(&phdr, phoff, sizeof(phdr));
+
+        // 打印 segment 的信息
+        printf("Segment %d: type=%d, offset=0x%x, vaddr=0x%x, paddr=0x%x, filesz=%d, memsz=%d\n",
+               i, phdr.p_type, phdr.p_offset, phdr.p_vaddr, phdr.p_paddr, phdr.p_filesz, phdr.p_memsz);
+    }
+    assert(0);
+    return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
