37bdc718bb8267b72f7d8f4ee98a61ba235977f1
tracer-ics2023 <tracer@njuics.org>
2023-12-14 16:12:14 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:14 up  1:26,  1 user,  load average: 1.27, 0.88, 0.74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,34 @@
 #include <common.h>
 #include "syscall.h"
-#define SYS_yield 1
+enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
     case SYS_yield: yield(); c->GPRx = 0; break;
+    case SYS_exit: halt(c->GPR2); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
