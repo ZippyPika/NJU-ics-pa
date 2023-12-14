a34621d4fd3d9db4e272df8be99b58dc7e7dc8e2
tracer-ics2023 <tracer@njuics.org>
2023-12-14 17:22:04 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:04 up  2:36,  1 user,  load average: 1.09, 0.96, 0.69

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,7 +5,6 @@ int _sys_write(int fd, void *buf, size_t count){
     if(fd!=1&&fd!=2){
         return -1;
     }
-    printf("write %d\n",count);
     char *str=(char *)buf;
     for(int i=0;i<count;i++){
         putch(str[i]);
