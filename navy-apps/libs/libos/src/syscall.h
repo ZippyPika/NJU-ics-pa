4d5c057702f9e03872b995db49ca1ad964a838d7
tracer-ics2023 <tracer@njuics.org>
2023-12-15 21:54:19 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:19 up  4:36,  1 user,  load average: 2.37, 1.29, 0.87

--- navy-apps/libs/libos/src/syscall.h
+++ navy-apps/libs/libos/src/syscall.h
@@ -31,4 +31,3 @@ size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
-/////
