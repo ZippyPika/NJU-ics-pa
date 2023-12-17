4dcf7e354104699735688ad6017721fcc4ee1ff8
tracer-ics2023 <tracer@njuics.org>
2023-12-17 20:28:37 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:28:37 up 41 min,  1 user,  load average: 1.11, 0.98, 0.84

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -3,23 +3,15 @@
 #include <sys/time.h>
 
 int main() {
-    struct timeval init;
-    struct timeval now;
-    printf("fuck\n");
-    assert(gettimeofday(&init, NULL) == 0);
-    time_t init_sec = init.tv_sec;
-    suseconds_t init_usec = init.tv_usec;
-
-    size_t times = 1;
-
-    while (1) {
-        assert(gettimeofday(&now, NULL) == 0);
-        time_t now_sec = now.tv_sec;
-        suseconds_t now_usec = now.tv_usec;
-        uint64_t time_gap = (now_sec - init_sec) * 1000000 + (now_usec - init_usec); // unit: us
-        if (time_gap > 500000 * times) {
-            printf("Half a second passed, %u time(s)\n", times);
-            times++;
-        }
+  struct timeval tv;
+  // struct timezone tz;
+  gettimeofday(&tv, NULL);
+  __uint64_t ms = 500;
+  while (1) {
+    while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
+      gettimeofday(&tv, NULL);
     }
+    ms += 500;
+    printf("ms = %d\n", ms);
+  }
 }
