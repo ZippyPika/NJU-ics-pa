0a26870c8a42109dc5ef5b758358c6c507a417e2
tracer-ics2023 <tracer@njuics.org>
2023-12-17 20:54:37 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:37 up  1:07,  1 user,  load average: 2.17, 1.52, 1.10

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -7,6 +7,7 @@ int main() {
   // struct timezone tz;
   gettimeofday(&tv, NULL);
   __uint64_t ms = 1000;
+  printf("ms = %ld sec=%ld usec=%ld \n", ms,tv.tv_sec,tv.tv_usec);
   while (1) {
     while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
       gettimeofday(&tv, NULL);
