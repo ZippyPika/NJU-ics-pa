52b69ad6ed575e29adffb949be83fc36d911b6b0
tracer-ics2023 <tracer@njuics.org>
2023-12-17 20:15:14 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:15:14 up 27 min,  1 user,  load average: 0.78, 0.88, 0.74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,22 @@
 #include <common.h>
 #include "syscall.h"
+#define time_t uint64_t
+#define suseconds_t uint64_t
+struct timeval {
+    time_t      tv_sec;     /* seconds */
+    suseconds_t tv_usec;    /* microseconds */
+};
 
+static uintptr_t sys_gettimeofday(uintptr_t *args) {
+  struct timeval *tv = (void *)args[0];
+  //struct timezone *tz = (void *)args[1];
+  uint64_t us=io_read(AM_TIMER_UPTIME).us;
+  if(tv!=NULL){
+      tv->tv_sec=us/1000000;
+      tv->tv_usec=us%1000000;
+  }
+  return 0;
+}
 void do_syscall(Context *c) {
     uintptr_t a[4];
     a[0] = c->GPR1;
@@ -16,6 +32,8 @@ void do_syscall(Context *c) {
         case SYS_read:c->GPRx=fs_read(a[1],(void*)a[2],a[3]);break;
         case SYS_close:c->GPRx=fs_close(a[1]);break;
         case SYS_lseek:c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
+        case SYS_gettimeofday:c->GPRx=sys_gettimeofday(a);break;
         default: panic("Unhandled syscall ID = %d", a[0]);
     }
 }
+
