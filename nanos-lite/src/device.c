1c2cb061364135bf268f7ee6ce9829f7ee54eb33
tracer-ics2023 <tracer@njuics.org>
2023-12-21 20:33:32 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:33:32 up  2:10,  1 user,  load average: 0.85, 1.33, 1.35

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -67,7 +67,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
     int weight=info.width;
     int y=offset/weight;
     int x=offset%weight;
-    io_write(AM_GPU_FBDRAW,x,y,(void *)buf,len,1,true);
+    io_write(AM_GPU_FBDRAW,x,y,(void *)buf,len/4,1,true);
     return len;
 }
 
