dd28d21e8de6496d4fc408ba0c53eb6ef55435e1
tracer-ics2023 <tracer@njuics.org>
2023-12-23 18:25:37 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:25:37 up 34 min,  1 user,  load average: 1.45, 1.43, 1.27

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,23 +12,20 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-    FILE * fp = fopen(filename, "r");
-    if (!fp) return NULL;
-
-    fseek(fp, 0L, SEEK_END);
-    long size = ftell(fp);
-
+    FILE *fp=fopen(filename,"r");
+    if(fp==NULL){
+        printf("open file %s failed\n",filename);
+        return NULL;
+    }
+    fseek(fp,0L,SEEK_END);
+    long size=ftell(fp);
     rewind(fp);
-    unsigned char * buf = (unsigned char *)malloc(size * sizeof(unsigned char));
-    assert(fread(buf, 1, size, fp) == size);
-
-    SDL_Surface * surface = STBIMG_LoadFromMemory(buf, size);
-    assert(surface != NULL);
-
-    fclose(fp);
+    unsigned char *buf=(unsigned char*)malloc(size*sizeof(unsigned char));
+    fread(buf,1,size,fp);
+    SDL_Surface *ret=STBIMG_LoadFromMemory(buf,size);
     free(buf);
-
-    return surface;
+    fclose(fp);
+    return ret;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
