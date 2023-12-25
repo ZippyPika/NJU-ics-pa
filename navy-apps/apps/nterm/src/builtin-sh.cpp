f4d76cf2026dfb4ec6a69e661f2a28203f885a97
tracer-ics2023 <tracer@njuics.org>
2023-12-25 19:30:18 +0800

    >  compile NEMU 221098038 张浩淼 Linux 550W 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:30:18 up  1:00,  1 user,  load average: 1.76, 1.68, 1.37

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -28,9 +28,9 @@ static void sh_handle_cmd(const char *cmd) {
     printf("cmd: %s\n", cmd);
     char *file_buf = (char *)malloc((strlen(cmd) + 1) * sizeof(char));
     memset(file_buf, 0, strlen(cmd) + 1);
-    file_buf=strncpy(file_buf,cmd,strlen(cmd)-1);
+    file_buf=strncpy(file_buf,cmd,strlen(cmd)-2);
     printf("1");
-    assert(0);
+    //assert(0);
     execvp(file_buf, NULL);
     return;
 }
